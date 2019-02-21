c9eb451f31aaef4211bb37f60c59cfe69bce9c54
mw@localhost.me <mw@localhost.me>
2019-02-21 16:37:22 +0100

    initial commit

---
+++ src/main/java/com/jakowaty/piotrbe/servlet/JakowatyFrontController.java
@@ -0,0 +1,32 @@
+package com.jakowaty.piotrbe.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class JakowatyFrontController
+ */
+public class JakowatyFrontController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public JakowatyFrontController() {
+        super();
+    }
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    	String contextPath = req.getContextPath();
+    	String requestMethod = req.getMethod();
+    	
+    	resp.getWriter().write(contextPath);
+    	resp.getWriter().write(requestMethod);
+    }
+}
+
+
