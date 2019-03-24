516822ef81999f02880383ef636df6755b36a0d2
mw@localhost.me <mw@localhost.me>
2019-03-24 06:17:46 +0100

    [WIP]

--- src/main/java/com/jakowaty/piotrbe/servlet/JakowatyFrontController.java
+++ src/main/java/com/jakowaty/piotrbe/servlet/JakowatyFrontController.java
@@ -1,32 +1,54 @@
 package com.jakowaty.piotrbe.servlet;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
+
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import com.jakowaty.piotrbe.router.JakowatyRouter;
 
-/**
- * Servlet implementation class JakowatyFrontController
- */
-public class JakowatyFrontController extends HttpServlet {
+
+public class JakowatyFrontController extends HttpServlet
+{
 	private static final long serialVersionUID = 1L;
-       
+	private static final String applicationRoutesFile = "routes.json";
+    private JakowatyRouter router;
+    
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public JakowatyFrontController() {
+    public JakowatyFrontController() throws Exception
+    {
         super();
+        
+        InputStream routeMapStream = this.getClass()
+        		.getClassLoader()
+        		.getResourceAsStream(JakowatyFrontController.applicationRoutesFile);
+        
+        if (routeMapStream == null) {
+        	throw new FileNotFoundException("Routing map resource not found.");
+        }
+
+        this.router = new JakowatyRouter(routeMapStream, JakowatyFrontController.applicationRoutesFile);
     }
 
     @Override
-    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
+    {
     	String pathInfo = req.getPathInfo();
     	String requestMethod = req.getMethod();
-    	
-    	resp.getWriter().write(pathInfo);
-    	resp.getWriter().write(requestMethod);
+
+		//here we know router has map analyzer initiated
+		//and attempted to load stream - otherwise E would be thrown
+		resp.getWriter().append(this.router.routeMapAnalyzer.asString);
+		
+		resp.getWriter().append(this.router.routeMapAnalyzer.asString);
+    	if (this.router.find(requestMethod, pathInfo, resp.getWriter())) {
+    		
+    	}
     }
 }
-
-
