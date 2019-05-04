81331fe4730aa7539dd7f364031db29e1b0cdd86
mw@localhost.me <mw@localhost.me>
2019-05-04 21:50:35 +0200

    rework

--- src/main/java/com/jakowaty/piotrbe/router/InvalidMapFormatException.java
+++
@@ -1,8 +0,0 @@
-package com.jakowaty.piotrbe.router;
-
-public class InvalidMapFormatException extends Exception {
-	private static final long serialVersionUID = 1L;
-	public InvalidMapFormatException(String m) {
-		super(m);
-	}
-}
--- src/main/java/com/jakowaty/piotrbe/servlet/FrontController.java
+++ src/main/java/com/jakowaty/piotrbe/servlet/FrontController.java
@@ -1,55 +1,50 @@
 package com.jakowaty.piotrbe.servlet;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStream;
-
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import com.jakowaty.piotrbe.router.Router;
 
+import com.jakowaty.piotrbe.router.SimpleRouter;
 
+/**
+ * This class is about starting "application".
+ * Its role is only encapsulate application logic into servlet.
+ * It will intercept all requests and push them through Router (as stated in web.xml).
+ * Everything is encapsulated in one method - I want it to be portable.
+ * For this moment service() method of HttpServlet serves as hipotetical 
+ * Application class and its run() method. Everything is constructed and fired in few lines.
+ * 
+ * @author piotrbe|jakowaty <https://github.com/jakowaty>
+ */
 public class FrontController extends HttpServlet
 {
 	private static final long serialVersionUID = 1L;
-	/** @todo some kind of external config */
-	private static final String applicationRoutesFile = "routes.json";
-    private Router router;
-    
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public FrontController() throws Exception
-    {
-        super();
-        
-        InputStream routeMapStream = this.getClass()
-        		.getClassLoader()
-        		.getResourceAsStream(FrontController.applicationRoutesFile);
-        
-        if (routeMapStream == null) {
-        	throw new FileNotFoundException("Routing map resource not found.");
-        }
-
-        this.router = new Router(routeMapStream, FrontController.applicationRoutesFile);
-    }
 
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
     {
-    	String pathInfo = req.getPathInfo();
-    	String requestMethod = req.getMethod();
-
-		//here we know router has map analyzer initiated
-		//and attempted to load stream - otherwise E would be thrown
-//		resp.getWriter().append(this.router.routeMapAnalyzer.asString);
-		
-		resp.getWriter().append(this.router.routeMapAnalyzer.asString);
-    	if (this.router.find(requestMethod, pathInfo, resp.getWriter())) {
+    	try {
+    		SimpleRouter simpleRouter = new SimpleRouter();
     		
-    	}
+    		simpleRouter.get(
+    			"/test-param/{:param}",
+    			(request, response, router, route) -> {
+    				try {
+    					response.getWriter().append("aaa");
+    				} catch (Exception e) {
+						return false;
+					}
+    				return true;
+				}
+			);
+ 
+    		simpleRouter.run(req, resp);
+    	} catch (Exception e) {
+			// TODO: handle exception
+    		resp.getWriter().append("EXCEPTION: " + e.getMessage());
+		}
     }
 }
